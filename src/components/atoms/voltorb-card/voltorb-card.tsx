3c184b001a695838ce1f3132c31daac99826fa2f
Necitero <necitero@gmail.com>
2024-04-29 22:29:13 +0200

    fix(VoltorbCard): prevent number selection

--- src/components/atoms/voltorb-card/voltorb-card.tsx
+++ src/components/atoms/voltorb-card/voltorb-card.tsx
@@ -9,12 +9,12 @@ export const VoltorbCard = ({ points, bombs }: VoltorbCardProps) => {
     const pointString = points.toString()
     return (
         <div className={`voltorb-card`}>
-            <div className="voltorb-card__point-digit">
+            <div className="voltorb-card__point-digit noselect">
                 <span>
                     {pointString.length === 1 ? `0${pointString}` : pointString}
                 </span>
             </div>
-            <div className="voltorb-card__bomb-digit">
+            <div className="voltorb-card__bomb-digit noselect">
                 <div className="voltorb-card__sprite" />
                 <span>{bombs}</span>
             </div>
