fa3d746d7be44815abaa8891034f8b4a082d31c0
Necitero <necitero@gmail.com>
2024-05-01 23:00:12 +0200

    refactor: remove gameover and winscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import { PlayField } from './components/organisms/play-field/play-field'
 import './App.css'
-import { GameOverScreen } from './components/organisms/game-over-screen/game-over-screen'
-import { WinScreen } from './components/organisms/win-screen/win-screen'
+import { StatusScreen } from './components/organisms/status-screen/status-screen'
+import { Message } from './components/atoms/message/message'
+import { useState } from 'react'
 
 function App() {
+    // 'running' | 'gameover' | 'win'
+    const [gameState, setGameState] = useState('running')
+
+    document.addEventListener(
+        'game-status',
+        (e: CustomEvent) => {
+            const { detail } = e
+            setGameState(detail)
+        },
+        { once: true }
+    )
+
     return (
         <main>
             <div id="game">
                 <PlayField />
-                <GameOverScreen />
-                <WinScreen />
+                <StatusScreen active={gameState === 'win'}>
+                    <Message text="Board clear!" />
+                </StatusScreen>
+                <StatusScreen active={gameState === 'gameover'}>
+                    <Message text="You lost!" />
+                </StatusScreen>
             </div>
         </main>
     )
